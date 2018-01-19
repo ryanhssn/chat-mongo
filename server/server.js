2563a50fd28ad7f17d5fc974017677b261d940e9
Bilal H <hussain.bilal000786@gmail.com>
2018-01-19 17:54:41 +0530

    Add auth token to the user signup

--- server/server.js
+++ server/server.js
@@ -54,7 +54,9 @@ app.post('/users', (req, res) => {
 	var user = new User(body);
 
 	user.save().then((user) => {
-		res.send(user);
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
 	}).catch((e) => {
 		res.status(400).send(e)
 	})
