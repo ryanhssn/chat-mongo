3262fff669255f01e50bad404d4377c3078da156
Bilal H <hussain.bilal000786@gmail.com>
2018-01-21 01:05:56 +0530

    Add login error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/chat-mongo')
+//mongoose.connect('mongodb://localhost:27017/chat-mongo')
+mongoose.connect('mongodb://dbuser:dbpassword@ds111078.mlab.com:11078/chat-mongo-bh')
 
 module.exports = {mongoose};
